2aae935520d496aa50243423b45462f4353032c6
Edgar Mendez <edgar.ym26@gmail.com>
2022-06-17 19:27:20 -0400

    close to complete ahh

--- models/index.js
+++ models/index.js
@@ -6,11 +6,11 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category,{
-  foreignKey: 'product_id',
+  
 })
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id',
+  
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
